3d4c757599b763fcfc7975eecbb9b8aab7a3b78e
akashnema2023 <akashnema2023@gmail.com>
2023-04-19 17:42:47 +0530

    fifth commit here

--- src/main/java/com/org/MyController.java
+++ src/main/java/com/org/MyController.java
@@ -1,12 +1,14 @@
 package com.org;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/emp")
 public class MyController {
+
+    @Autowired
+    UserRepo userRepo;
     @GetMapping("/view")
     public String home(){
         return "this is view page!!";
@@ -14,5 +16,11 @@ public class MyController {
     @GetMapping("/welcome")
     public String welcomes(){
         return "this is welcome page!!";
+    }
+
+    @PostMapping("/add")
+    public String addUser(@RequestBody UserEntity userEntity){
+        userRepo.save(userEntity);
+        return "successfully add user in your db!!";
     }
 }
